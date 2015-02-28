d20150917ce09ae8f55ce4cce315312401bfa17b
Sean <seansan123@gmail.com>
2015-02-28 14:49:11 +0000

    Removed Test data

--- index.php
+++ index.php
@@ -2,36 +2,9 @@
 <html>
 <head>
     <title>TecInASec</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 </head>
 <body>
-	<div class="navbar navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<a class="brand" href="index.php">TecInASec</a>
-				<ul class="nav">
-					<li class="active">
-						<a href="index.php">Basic</a>
-					</li>
-					<li>
-						<a href="advanced.php">Advanced</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-
-	<div class="container">
-		<div class="col-md-4 col-md-offset-4">
-			<form>
-				<h2 class="form-signin-heading">Test</h2>
-				<input class="input-block-level" type="text" placeholder="Test" ></input>
-			</form>
-		</div>
-	</div>
-
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="js/bootstrap.js"></script>
+	
 </body>
 </html>
