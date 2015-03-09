dd7725455b545bc0891fa69c0a9bfabeb728e3b7
Sean <seansan123@gmail.com>
2015-03-09 12:12:59 +0000

    Access still denied

--- search.php
+++ search.php
@@ -1,11 +1,18 @@
 <?php
-	$link = mysqli_connect('127.8.149.130', 'adminFY55S2j', 'yTDlCJyDTwWs', 'tecinasectemp', '3306');
 
-	if (!$link) {
+	$dbIp = "127.8.149.130";
+	$dbUser = "adminFY55S2j";
+	$dbPass = "yTDlCJyDTwWs";
+	$dbName = "tecinasectemp";
+	$dbPort = 3306;
+
+	$conn = mysqli_connect($dbIp, $dbUser, $dbPass, $dbName, $dbPort);
+
+	if (!$conn) {
 	    die('Connection Error: ('.mysqli_connect_errno().')'.mysqli_connect_error());
 	}
 
-	echo 'Success... ' . mysqli_get_host_info($link) . "\n";
+	echo 'Success... ' . mysqli_get_host_info($conn) . "\n";
 
-	mysqli_close($link);
+	mysqli_close($conn);
 ?>
