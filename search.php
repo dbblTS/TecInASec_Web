6cf43f3eabcc82b578c985cb39e9441719b481af
Sean <seansan123@gmail.com>
2015-03-09 11:58:52 +0000

    Working connection, access denied

--- search.php
+++ search.php
@@ -1,24 +1,11 @@
-<?php 
-	define('DB_HOST', getenv('127.8.149.130'));
-	define('DB_PORT', getenv('3306'));
-	define('DB_USER', getenv('adminFY55S2j'));
-	define('DB_PASS', getenv('yTDlCJyDTwWs'));
-	define('DB_NAME', getenv('tecinasectemp'));
+<?php
+	$link = mysqli_connect('127.8.149.130', 'adminFY55S2j', 'yTDlCJyDTwWs', 'tecinasectemp', '3306');
 
-	$dbhost = constant("DB_HOST"); // Host name 
-	$dbport = constant("DB_PORT"); // Host port
-	$dbusername = constant("DB_USER"); // Mysql username 
-	$dbpassword = constant("DB_PASS"); // Mysql password 
-	$db_name = constant("DB_NAME"); // Database name 
-
-	$MySQLConnect = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($MySQLConnect));
-	mysqli_select_db($MySQLConnect, $db_name) or die("Error: " . mysqli_error($MySQLConnect));
-
-	if ($MySQLConnect == false) {
-		echo "Not connected...";
-	}
-	else {
-		echo "Connected!!";
+	if (!$link) {
+	    die('Connection Error: ('.mysqli_connect_errno().')'.mysqli_connect_error());
 	}
 
+	echo 'Success... ' . mysqli_get_host_info($link) . "\n";
+
+	mysqli_close($link);
 ?>
